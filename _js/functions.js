b3918ba20eb9b6afa959f506149ad6df4e0ba7a9
ericsatoru <41448835+ericsatoru@users.noreply.github.com>
2019-04-11 08:33:54 -0300

    satorusuzuki

---
+++ _js/functions.js
@@ -0,0 +1,14 @@
+function linkhome(){
+            window.location="index.html";
+}
+
+
+function linklavita(){
+            window.location="projeto-lavita.html";
+}
+
+function linkhenry(){
+            window.location="projeto-lavita.html";
+}
+
+
