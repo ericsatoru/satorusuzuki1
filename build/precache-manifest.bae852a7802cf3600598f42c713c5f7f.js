0b6777128af1d5f35bbcf2ba62f83ec7f848256e
ericsatoru <41448835+ericsatoru@users.noreply.github.com>
2020-01-19 19:00:41 -0300

    1

---
+++ build/precache-manifest.bae852a7802cf3600598f42c713c5f7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303e8fb8e99bb03e0fc5ff72961e672a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87cbdce032a6c45c6ab8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9fbd34f6e4796a70ce8f",
+    "url": "/static/js/2.0a8ac8cd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0a8ac8cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "87cbdce032a6c45c6ab8",
+    "url": "/static/js/main.180cbe35.chunk.js"
+  },
+  {
+    "revision": "7443ea878ce967456840",
+    "url": "/static/js/runtime-main.97de371d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
