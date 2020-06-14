e2263c23b38c5993df0ca2b33b1dcd5cdb2dde0d
ericsatoru <41448835+ericsatoru@users.noreply.github.com>
2020-06-14 19:54:19 -0300

    React v1

---
+++ backup/build/precache-manifest.e709caa3632e141b0feeca5f680f3b68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8decfa356c2e283d1255e1e1fbab63d9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "41146469c93b04320ebe",
+    "url": "./static/js/2.965400de.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.965400de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8cf45642f24ef852115e",
+    "url": "./static/js/main.12ceb269.chunk.js"
+  },
+  {
+    "revision": "73aeb752b4135f3e6670",
+    "url": "./static/js/runtime-main.dc28ed30.js"
+  }
+]);
---
+++ precache-manifest.932bc9ae209fb946d59e14ae20e72f99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc47de0211645865045cb0eb1ada12cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a600d97e5fbeaae88bd",
+    "url": "/static/css/main.c9de10e0.chunk.css"
+  },
+  {
+    "revision": "c2252d5170ba3771ea58",
+    "url": "/static/js/2.14131bb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.14131bb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a600d97e5fbeaae88bd",
+    "url": "/static/js/main.5a9362da.chunk.js"
+  },
+  {
+    "revision": "acc5cd3c5510d05bedea",
+    "url": "/static/js/runtime-main.6fd9f07f.js"
+  },
+  {
+    "revision": "960598a5dbbbcf4df0a98c3c57fb1d13",
+    "url": "/static/media/close.960598a5.svg"
+  }
+]);
